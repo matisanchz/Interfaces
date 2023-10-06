71756a3159bcfbdf5ec627b160a7000e32f3c7f7
Franco Delucchi <franco.delucchi@assertsolutions.com>
2023-10-06 01:54:29 -0300

    avances game-view

--- TP2/EntregaFinal/js/game.js
+++ TP2/EntregaFinal/js/game.js
@@ -6,7 +6,14 @@ canvas.height = 600;
 
 const ctx = canvas.getContext('2d');
 
-ctx.fillStyle = 'orange';
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+//ctx.fillStyle = 'orange';
+//ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 ctx.stroke();
+
+let img = new Image();
+img.src = "media/imagenes/game2.svg";
+
+img.onload = function(){
+    ctx.drawImage(img, 0, 0);
+  }
