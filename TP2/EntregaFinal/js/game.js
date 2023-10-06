e247ad7555034c5410961923257e99ad9bb35e65
Franco Delucchi <franco.delucchi@assertsolutions.com>
2023-10-06 17:38:15 -0300

    pop-up y animacion

--- TP2/EntregaFinal/js/game.js
+++ TP2/EntregaFinal/js/game.js
@@ -17,3 +17,16 @@ img.src = "media/imagenes/game2.svg";
 img.onload = function(){
     ctx.drawImage(img, 0, 0);
   }
+
+// Función para mostrar el pop-up cuando se hace clic en el botón "Compartir"
+document.getElementById('share').addEventListener('click', function() {
+  document.getElementById('popup-overlay').style.display = 'block';
+  // Generar el enlace a compartir y mostrarlo en el campo de texto
+  var shareLink = window.location.href; // Cambia esto por el enlace que deseas compartir
+  document.getElementById('share-link').value = shareLink;
+});
+
+// Función para cerrar el pop-up cuando se hace clic en el botón "Cerrar"
+document.getElementById('close-popup').addEventListener('click', function() {
+  document.getElementById('popup-overlay').style.display = 'none';
+});
