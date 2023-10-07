c02420bbe65acf18f8da95a6cb2c35c39666b63f
Matias Sanchez Abrego <matiassanchezabrego@gmail.com>
2023-10-07 15:55:49 -0300

    Se agrega preloader

--- TP2/EntregaFinal/js/form.js
+++ TP2/EntregaFinal/js/form.js
@@ -19,3 +19,50 @@ document.querySelector(".registro").addEventListener("click", function(){
     formLogin.classList.toggle("none");
     formRegister.classList.toggle("none");
 })
+
+document.addEventListener('DOMContentLoaded', loadPage);
+
+function loadPage() {
+
+    let preloader = document.createElement("div");
+    let ring = document.createElement("div");
+    let loading = document.createElement("span");
+    let percentage = document.createElement("span");
+
+    formLogin.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        preloader.classList.add("preloader");
+        ring.classList.add("ring");
+        percentage.classList.add("status");
+        loading.textContent = "Cargando...";
+
+        percentage.appendChild(document.createTextNode("0%"));
+
+        preloader.appendChild(ring);
+        preloader.appendChild(loading);
+        preloader.appendChild(percentage);
+
+        document.querySelector("body").appendChild(preloader);
+
+        activatePreloader();
+    });
+
+    function activatePreloader(){
+
+        let spinnerStatus = document.querySelector(".status");
+        document.querySelector("body").classList.add("form-hide");
+
+        setTimeout(() => {
+            window.location.replace("home.html");
+        }, 5000);
+
+        let status = 10;
+        setInterval(() => {
+            spinnerStatus.innerHTML = `${status}%`;
+            if(status < 100)
+                status += 5;
+        }, 242)
+    }
+    
+}
