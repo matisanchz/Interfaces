6e96a546a19b43128918ec42bf9a30157d065059
Matias Sanchez Abrego <matiassanchezabrego@gmail.com>
2023-11-20 17:40:39 -0300

    Header menu

---
+++ TP4/EntregaFinal/js/main.js
@@ -0,0 +1,34 @@
+"use strict";
+
+let isOnMenu = false;
+
+let hambur = document.querySelector(".hambur");
+
+hambur.addEventListener('mouseenter', showCross);
+
+function showCross(){
+    let firstLine = (hambur.children[0]).firstElementChild;
+    let mediumLine = (hambur.children[1]).firstElementChild;
+    let lastLine = (hambur.children[2]).firstElementChild;
+    let classes = Array.from(mediumLine.classList);
+    if(!classes.includes("hide")){
+        mediumLine.classList.add("hide");
+        firstLine.classList.add("shadow-disappear");
+        lastLine.classList.add("shadow-disappear");
+    }
+}
+
+hambur.addEventListener('mouseleave', hideCross);
+
+function hideCross(){
+    let firstLine = (hambur.children[0]).firstElementChild;
+    let mediumLine = (hambur.children[1]).firstElementChild;
+    let lastLine = (hambur.children[2]).firstElementChild;
+    console.log(firstLine);
+    let classes = Array.from(mediumLine.classList);
+    if(classes.includes("hide")){
+        mediumLine.classList.remove("hide");
+        firstLine.classList.remove("shadow-disappear");
+        lastLine.classList.remove("shadow-disappear");
+    }
+}
