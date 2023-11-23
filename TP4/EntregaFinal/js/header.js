1e15e3e5055f6b457644a368493991b0487ab72f
Matias Sanchez Abrego <matiassanchezabrego@gmail.com>
2023-11-23 19:33:08 -0300

    Sprites and logo

--- TP4/EntregaFinal/js/header.js
+++ TP4/EntregaFinal/js/header.js
@@ -10,10 +10,12 @@ window.addEventListener('scroll', function() {
     let scale = 1 - scrollY / 140;
 
     if(scale>0.2){
+        logo.style = 'position: absolute';
         logo.style.transform = 'translateY(' + translateY + 'px) ';
         logo.style.transform = 'scale(' + scale + ')';
     }else{
-        logo.style.transform = 'translateY(-185px) scale(0.2)';
+        logo.style = 'position: fixed';
+        logo.style.transform = `translateY(-107px) scale(0.20)`;
     }
 
 });
